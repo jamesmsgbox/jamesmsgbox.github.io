b61fcfb2ef77bdb328a856c5e41aff003ba84742
harold jhan <haroldzhen531@gmail.com>
2020-05-26 23:21:42 +0800

    超級櫻文第一版

--- app.js
+++ app.js
@@ -1,41 +1,88 @@
 const cards = [
   {
-    front: 'The "First Computer Programmer"',
+    front: '到了電影院，電影已經開始了。',
     back: 'Ada Lovelace',
     flipped: false,
   },
   {
-    front: 'Invented the "Clarke Calculator"',
+    front: '如果我很有錢，我會買很多汽車給你',
     back: 'Edith Clarke',
     flipped: false,
 
   },
-  {
-    front: 'Famous World War II Enigma code breaker',
-    back: 'Alan Turing',
-    flipped: false,
-  },
-  {
-    front: 'Created satellite orbit analyzation software for NASA',
-    back: 'Dr. Evelyn Boyd Granville',
-    flipped: false,
-  },
-]; 
+  // {
+  //   front: 'Famous World War II Enigma code breaker',
+  //   back: 'Alan Turing',
+  //   flipped: false,
+  // },
+  // {
+  //   front: 'Created satellite orbit analyzation software for NASA',
+  //   back: 'Dr. Evelyn Boyd Granville',
+  //   flipped: false,
+  // },
+  //   {
+  //       front: 'The "First Computer Programmer"',
+  //       back: 'Ada Lovelace',
+  //       flipped: false,
+  //   },
+  //   {
+  //       front: 'Invented the "Clarke Calculator"',
+  //       back: 'Edith Clarke',
+  //       flipped: false,
+  //
+  //   },
+  //   {
+  //       front: 'Famous World War II Enigma code breaker',
+  //       back: 'Alan Turing',
+  //       flipped: false,
+  //   },
+  //   {
+  //       front: 'Created satellite orbit analyzation software for NASA',
+  //       back: 'Dr. Evelyn Boyd Granville',
+  //       flipped: false,
+  //   },
+  //   {
+  //       front: 'The "First Computer Programmer"',
+  //       back: 'Ada Lovelace',
+  //       flipped: false,
+  //   },
+  //   {
+  //       front: 'Invented the "Clarke Calculator"',
+  //       back: 'Edith Clarke',
+  //       flipped: false,
+  //
+  //   },
+  //   {
+  //       front: 'Famous World War II Enigma code breaker',
+  //       back: 'Alan Turing',
+  //       flipped: false,
+  //   },
+  //   {
+  //       front: 'Created satellite orbit analyzation software for NASA',
+  //       back: 'Dr. Evelyn Boyd Granville',
+  //       flipped: false,
+  //   },
+];
 
 new Vue({
   el: '#flashcard-app',
   data: {
+    mCount: 10,
     cards: cards,
     newFront: '',
     newBack: '',
-    error: false
+    allcontent: '',
+    questioncode: '',
+    error: false,
+    isShow: true,
   },
   methods: {
     toggleCard: function(card){
       card.flipped = !card.flipped;
+        // console.log(JSON.stringify(this.allcontent));
     },
     addNew: function(){
-      //!this.newFront || !this.newBack also works
+      // !this.newFront || !this.newBack also works
       if(!this.newFront.length || !this.newBack.length){
         this.error = true;
       } else {
@@ -47,7 +94,36 @@ new Vue({
         this.newFront = '';
         this.newBack = '';
         this.error = false;
+        this.questioncode = JSON.stringify(this.cards);
       }
+    },
+    keepContent: function(index){
+        localStorage.setItem("keep"+index, JSON.stringify(this.cards));
+        this.questioncode = JSON.stringify(this.cards);
+    },
+    GetContent: function(index){
+        this.cards = "";
+        this.cards = this.shuffle(JSON.parse(localStorage.getItem("keep"+index)));
+        this.questioncode = JSON.stringify(this.cards);
+
+    },
+    Loading: function(){
+        this.cards = "";
+        this.cards = this.shuffle(JSON.parse(this.allcontent));
+        this.questioncode = JSON.stringify(this.cards);
+    },
+    getShow: function(){
+      this.isShow = !this.isShow;
+    },
+    shuffle: function(sourceArray){
+        for (var i = 0; i < sourceArray.length - 1; i++) {
+            var j = i + Math.floor(Math.random() * (sourceArray.length - i));
+
+            var temp = sourceArray[j];
+            sourceArray[j] = sourceArray[i];
+            sourceArray[i] = temp;
+        }
+        return sourceArray;
     }
   }
 });
