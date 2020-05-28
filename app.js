8852714fca4b7d0396c77f14734337666e33990d
Harold <tomer.rd07@gmail.com>
2020-05-28 12:02:06 +0800

    feat: 三重卡、隨選卡片

--- app.js
+++ app.js
@@ -18,53 +18,53 @@ const cards = [
   {
     front: '如果天塌下來，我就告訴你一個大秘密。',
     back: 'If the sky were to fall, I would tell you the biggest secret.',
-    flipped: false,
+    flipped: 1,
   },
   {
     front: '如果John 昨天晚上有穿外套，他今天就不會感冒了。',
     back: 'If John had worn a jacket last night, he wouldn\'t caught a cold now.',
-    flipped: false,
+    flipped: 1,
 
   },  {
     front: '如果你站在我的立場，你當時可能不會這樣說。',
     back: 'If you had been in my shoes, you might not have said so.',
-    flipped: false,
+    flipped: 1,
   },{
     front: '如果他認真讀書，他考試就會考得很好。',
     back: 'If he had studied hard, he would have done well on the test.',
-    flipped: false,
+    flipped: 1,
   },{
     front: '如果這裡沒有鳥跟花，我就會覺得很無聊',
     back: 'If there were no birds and flowers, I would feel bored.',
-    flipped: false,
+    flipped: 1,
   },{
     front: '如果我現在沒有很忙，我就會幫你。',
     back: 'If I were not so busy now, I would help you.',
-    flipped: false,
+    flipped: 1,
   },{
     front: '如果你學習更小心，你可以避免犯相同的錯。',
     back: 'If you study more carefully, you can avoid making the same mistake.',
-    flipped: false,
+    flipped: 1,
   },{
     front: 'Cherry不擅長韓文。她多希望她韓文很好。',
     back: 'Cherry isn\'t good at Korean. How she wishes she were good at it.',
-    flipped: false,
+    flipped: 1,
   },{
     front: '如果我昨天有時間，我就能和你出去逛街了。',
     back: 'If I had had time yesterday, I could have gone shopping with you.',
-    flipped: false,
+    flipped: 1,
   },{
     front: '如果當時我們沒在十點前離開，我們可能就錯過班機了。',
     back: 'If we hadn\'t before left ten o\'clock at that time, we could have missed　the fight.',
-    flipped: false,
+    flipped: 1,
   },{
     front: '如果我是老闆，我就會幫大家加薪(give everyone a raise)。',
     back: 'If I were a boss, I would give everyone a raise.',
-    flipped: false,
+    flipped: 1,
   },{
     front: 'Mary現在很窮，她希望她可以中樂透(win the lottery)',
     back: 'Mary is very poor now, she wishes she could win the lottery.',
-    flipped: false,
+    flipped: 1,
   },
 ];
 
@@ -72,7 +72,7 @@ const notYetCard = [
   {
     front: '尚無資料',
     back: 'Not yet',
-    flipped: false,
+    flipped: 1,
   },
 ];
 
@@ -87,13 +87,24 @@ var vm = new Vue({
     questioncode: '',
     error: false,
     isShow: false,
+    pickUpText: 'Pick Me',
+    pickUpNumber: 0,
   },
   created: function () {
     this.cards = cards;
+    this.randomNumber();
+    console.log(this.pickUpNumber);
   },
   methods: {
     toggleCard: function(card){
-      card.flipped = !card.flipped;
+      card.flipped = card.flipped + 1;
+      if (card.flipped > 3) {
+        card.flipped = 1;
+      }
+    },
+    randomNumber: function(){
+      this.pickUpNumber =Math.floor(Math.random()*this.cards.length);
+      console.log(this.pickUpNumber);
     },
     addNew: function(){
       // !this.newFront || !this.newBack also works
@@ -103,7 +114,7 @@ var vm = new Vue({
         this.cards.push({
           front: this.newFront,
           back: this.newBack,
-          flipped: false
+          flipped: 1
         });
         this.newFront = '';
         this.newBack = '';
